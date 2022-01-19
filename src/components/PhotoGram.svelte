8c1c0a85cde0f912fb1a8d25f61e3efa73e95bf8
TotalLag <phanx068@gmail.com>
2022-01-19 01:22:58 -0600

    init

---
+++ src/components/PhotoGram.svelte
@@ -0,0 +1,12 @@
+<script>
+  export let photos, helpers;
+</script>
+
+<div
+  class="relative w-64 h-64 overflow-hidden transition duration-300 ease-in-out bg-center bg-cover rounded-lg shadow-lg lg:w-64 lg:h-64 md:w-32 md:h-32 shrink-0 group hover:shadow-2xl"
+  style="background-image: url('{photos}');"
+>
+  <div class="relative flex items-end justify-center w-full h-full px-4 py-3">
+    <h3 class="text-center" />
+  </div>
+</div>
