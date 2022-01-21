99e8fba41dd197b03ec3f99fc88b1015c1ebe65c
TotalLag <phanx068@gmail.com>
2022-01-21 02:01:19 -0600

    feat: added protip shortcode for blogging

--- src/shortcodes.js
+++ src/shortcodes.js
@@ -58,4 +58,25 @@ module.exports = [
       };
     },
   },
+  {
+    shortcode: 'protip',
+    run: async ({ content }) => {
+      // allRequests represents 'request' objects for all of the pages of our site, if we know the length of that we know the length of our site.
+      return {
+        html: `<div class="flex items-center bg-blue-400 border-l-4 border-blue-700 py-2 px-3 shadow-md mb-2">
+                <div class="text-blue-500 rounded-full bg-white mr-3">
+                  <svg width="1.8em" height="1.8em" viewBox="0 0 16 16" class="bi bi-info" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+                    <path d="M8.93 6.588l-2.29.287-.082.38.45.083c.294.07.352.176.288.469l-.738 3.468c-.194.897.105 1.319.808 1.319.545 0 1.178-.252 1.465-.598l.088-.416c-.2.176-.492.246-.686.246-.275 0-.375-.193-.304-.533L8.93 6.588z"/>
+                    <circle cx="8" cy="4.5" r="1"/>
+                  </svg>
+                </div>
+
+                <div class="text-white">
+                  ${content}
+                </div>
+              </div>`,
+        // other values can be omitted.
+      };
+    },
+  },
 ];
