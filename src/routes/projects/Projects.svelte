ce3215669f5dbde3b95c137f541df4d57f3c46c1
TotalLag <phanx068@gmail.com>
2022-01-24 16:46:13 -0600

    wip

--- src/routes/projects/Projects.svelte
+++ src/routes/projects/Projects.svelte
@@ -2,8 +2,39 @@
   export let data;
 </script>
 
-Hello Project
-
-{#each data.markdown.project as project}
-  {JSON.stringify(project)}
-{/each}
+<div class="mx-auto min-w-fit sm:w-5/6">
+  <div class="my-6 bg-white rounded shadow-md min-w-fit">
+    <table class="w-full table-auto">
+      <thead class="border">
+        <tr class="text-center uppercase bg-gray-100">
+          <th class="px-2 py-3">Project</th>
+          <th class="px-2 py-3">What</th>
+          <th class="px-2 py-3">Why</th>
+          <th class="px-2 py-3 sm:w-60">Tech</th>
+        </tr>
+      </thead>
+      <tbody class="text-sm font-light">
+        <tr>
+          <td class="px-2 py-3 text-center whitespace-nowrap shrink">
+            <div class="flex items-center justify-center">
+              <span class="font-medium">TBD</span>
+            </div>
+          </td>
+          <td class="px-2 py-3 text-center shrink">
+            <div class="flex items-center justify-center">
+              <span>TBD</span>
+            </div>
+          </td>
+          <td class="px-2 py-3 text-center grow">
+            <div class="flex items-center justify-center">
+              <span>TBD</span>
+            </div>
+          </td>
+          <td class="px-2 py-3 text-center shrink">
+            <span class="px-3 py-1 text-xs text-purple-600 bg-purple-200 rounded-full">TBD</span>
+          </td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
+</div>
