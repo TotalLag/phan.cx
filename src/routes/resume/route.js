8c706b760bd1f46c75b1d96e603e5a5301c02825
TotalLag <phanx068@gmail.com>
2022-02-04 19:08:22 -0600

    chore: update routing rules

--- src/routes/resume/route.js
+++ src/routes/resume/route.js
@@ -1,10 +1,17 @@
 module.exports = {
   // the all function returns an array of all of the 'request' objects of a route. Since this is the homepage, there is only one.
-  all: () => [{ slug: 'resume' }],
+  all: () => [
+      { slug: 'resume', resume: { type: '' } },
+      { slug: 'resume', resume: { type: 'tactical' } },
+    ],
   // the permalink function takes a 'request' object and returns a relative permalink. In this case "/"
-  permalink: ({ request }) => request.slug,
-  data: ({ data }) => {
-    const { resume } = require('./resume.json');
+  permalink: ({ request }) => `${request.slug}/${request.resume.type}`,
+  data: ({ request, data }) => {
+    let { resume } =  require('./resume.json');
+
+    if (request.resume.type === 'tactical') {
+      resume = require('./resume-tech.json').resume;
+    }
 
     return { ...data, resume };
   },
