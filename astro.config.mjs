24e8f031defb94a051f97f8dbd5ab0d5d4b35044
TotalLag <phanx068@gmail.com>
2024-11-11 01:31:54 -0600

    update code

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,4 @@
-import { defineConfig, envField } from 'astro/config';
+import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import mdx from '@astrojs/mdx';
 import solid from '@astrojs/solid-js';
